27aab5f2960270a639c3c5ed6884f78163cb907c
Amanda Liu <amandastephanieliu@gmail.com>
2021-03-18 20:25:41 -0400

    update App.js, Places.js, AirportInfo.js

--- starter-app/src/App.js
+++ starter-app/src/App.js
@@ -1,12 +1,35 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      Starter Code
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello World!</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+
+// import React from 'react';
+// import './App.css';
+// import AirportInfo from './AirportInfo';y
+// import Places from './Places';
+// import Header from './Header';
+// import Footer from './Footer';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Header />
+//       <h1>header</h1>
+//       <AirportInfo />
+//       <Places />
+//       <Footer />
+//     </div>
+//   );
+// }
+
+// export default App;
